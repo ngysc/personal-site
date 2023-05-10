ca42a298b19c42c638f22bde3ab5b3225634dd7b
Sarolta Nagy <sarolta.nagy14@gmail.com>
2023-05-10 14:11:19 +0200

    refactor: rewrite themeContextWrapper to detect system preference

--- src/theme/themeContextWrapper.js
+++ src/theme/themeContextWrapper.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import { ThemeContext, themes } from "../contexts/themeContext";
 
 export default function ThemeContextWrapper(props) {
-  const [theme, setTheme] = useState(themes.dark);
+  const [theme, setTheme] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? themes.dark
+      : themes.light
+  );
 
   function changeTheme(theme) {
     setTheme(theme);
@@ -11,14 +15,25 @@ export default function ThemeContextWrapper(props) {
   useEffect(() => {
     switch (theme) {
       case themes.light:
-        document.body.classList.add("white-content");
+        document.body.classList.remove("white-content");
         break;
       case themes.dark:
       default:
-        document.body.classList.remove("white-content");
+        document.body.classList.add("white-content");
         break;
     }
   }, [theme]);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleMediaQueryChange = (event) => {
+      setTheme(event.matches ? themes.dark : themes.light);
+    };
+    mediaQueryList.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQueryList.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme: theme, changeTheme: changeTheme }}>
